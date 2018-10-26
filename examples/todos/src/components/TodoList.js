fe8b95b554badac50efca65154404ac7d6556e26
theSouthSea <1611185386@qq.com>
2018-10-26 15:17:16 +0800

    修改todoList

--- examples/todos/src/components/TodoList.js
+++ examples/todos/src/components/TodoList.js
@@ -2,25 +2,25 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Todo from './Todo'
 
-const TodoList = ({ todos, toggleTodo }) => (
-  <ul>
-    {todos.map(todo =>
-      <Todo
-        key={todo.id}
-        {...todo}
-        onClick={() => toggleTodo(todo.id)}
-      />
-    )}
-  </ul>
-)
+const TodoList = ({todos, toggleTodo}) => {
+    console.log('todos', todos);
+    return (
+        <ul>
+          { todos.map(todo => {
+                return (<Todo key={ todo.id } {...todo} onClick={ () => toggleTodo(todo.id) } />)
+            }
+            ) }
+        </ul>
+    )
+}
 
 TodoList.propTypes = {
-  todos: PropTypes.arrayOf(PropTypes.shape({
-    id: PropTypes.number.isRequired,
-    completed: PropTypes.bool.isRequired,
-    text: PropTypes.string.isRequired
-  }).isRequired).isRequired,
-  toggleTodo: PropTypes.func.isRequired
+    todos: PropTypes.arrayOf(PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        completed: PropTypes.bool.isRequired,
+        text: PropTypes.string.isRequired
+    }).isRequired).isRequired,
+    toggleTodo: PropTypes.func.isRequired
 }
 
 export default TodoList
